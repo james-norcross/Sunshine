eeb6f5655059d1bdc3d90301b4f9415475af6c87
james-norcross <james.norcross@gmail.com>
2016-08-18 09:21:19 -0500

    Add array adapter for forecast data and bind it to listview to display it

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -22,14 +24,19 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         // fake data
-        ArrayList<String> fakeData = new ArrayList<String>();
+        ArrayList<String> weekForecast = new ArrayList<String>();
 
-        fakeData.add("Today - Cloudy - 83/65");
-        fakeData.add("Tomorrow - Thunderstorms - 81/63");
-        fakeData.add("Saturday - Partly Cloudy - 83/65");
-        fakeData.add("Sunday - Cloudy - 83/65");
-        fakeData.add("Monday - Isolated Thunderstorms - 83/65");
-        fakeData.add("Tuesday - Cloudy - 83/65");
+        weekForecast.add("Today - Cloudy - 83/65");
+        weekForecast.add("Tomorrow - Thunderstorms - 81/63");
+        weekForecast.add("Saturday - Partly Cloudy - 83/65");
+        weekForecast.add("Sunday - Cloudy - 83/65");
+        weekForecast.add("Monday - Isolated Thunderstorms - 83/65");
+        weekForecast.add("Tuesday - Cloudy - 83/65");
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forcast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
 
         return rootView;
     }
